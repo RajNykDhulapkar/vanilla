ef5ac16cae9f4d746767cfd2821320e4d6b6644e
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2023-01-02 19:54:44 +0530

    -a

--- script.js
+++ script.js
@@ -14,3 +14,50 @@ input.addEventListener("keypress", function (event) {
         // document.getElementById("myBtn").click();
     }
 });
+
+// const isHover = e => !!(e.querySelector(":hover") || e.parentElement.querySelector(":hover") === e);
+
+// const myDiv = document.getElementById('card_1');
+// const cards = document.querySelectorAll('.card');
+// const cards = document.getElementsByClassName('card');
+// cast to array
+// const cards = Array.from(document.getElementsByClassName('card'));
+// // select all cards with class card
+// console.log(cards);
+
+// document.addEventListener('mousemove', function checkHover() {
+//     cards.forEach(card => {
+//         const hovered = isHover(card);
+//         if (hovered !== checkHover.hovered) {
+//             // console.log(hovered ? 'hovered' : 'not hovered');
+//             if (hovered) {
+//                 card.classList.add("hovered");
+//                 console.log("hovered");
+//             } else {
+//                 card.classList.remove("hovered");
+//                 console.log("not hovered");
+//             }
+//             checkHover.hovered = hovered;
+//         }
+//     });
+// const hovered = isHover(myDiv);
+// if (hovered !== checkHover.hovered) {
+//     // console.log(hovered ? 'hovered' : 'not hovered');
+//     if (hovered) {
+//         myDiv.classList.add("hovered");
+//         console.log("hovered");
+//     } else {
+//         myDiv.classList.remove("hovered");
+//         console.log("not hovered");
+//     }
+//     checkHover.hovered = hovered;
+// }
+// });
+
+const cards = $('.card');
+
+cards.hover(function (elt) {
+    if (cards.index(document.activeElement) === -1) {
+        $(this).toggleClass('hovered');
+    }
+})
